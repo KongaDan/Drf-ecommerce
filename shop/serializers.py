ad4db1ce61aa9337fe0374d424c9887c67cdba27
Konga Dan <kongadan68@gmail.com>
2024-04-19 13:27:42 +0100

    Imbrication des serializer et pagination feat

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,18 +1,44 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from shop.models import Category, Product, Article
 
-class CategoySerializer(ModelSerializer):
-    class Meta:
-        model=Category
-        fields = ['id','name']
-
-class ProductSerializer(ModelSerializer):
-    class Meta:
-        model=Product
-        fields = ['id','date_created','date_updated','name','category_id']
-
 class ArticleSerializer(ModelSerializer):
+
     class Meta:
         model= Article
         fields =['id','date_created','date_updated','name','price','product_id']
+
+"""
+Pour cela, nous pouvons redéfinir notre attribut de classe products 
+avec un SerializerMethodField  qui nous donne alors la possibilité de filtrer les produits à retourner.
+"""
+class ProductSerializer(ModelSerializer):
+
+    articles = serializers.SerializerMethodField()
+
+    class Meta:
+        model=Product
+        fields = ['id','date_created','date_updated','name','category_id','articles']
+
+    def get_articles(self, instance):
+        queryset = instance.articles.filter(active = True)
+        serializer = ArticleSerializer(queryset, many=True)
+        return serializer.data
+
+class CategoySerializer(ModelSerializer):
+    
+    #products = ProductSerializer(many= True)
+    products= serializers.SerializerMethodField()
+
+    class Meta:
+        model=Category
+        fields = ['id','date_created','date_updated','name', 'products']
+
+    #Pour retourner que le produits actifs
+    def get_products(self, instance):
+        queryset= instance.products.filter(active = True)
+
+        serializer= ProductSerializer(queryset, many=True)
+        return serializer.data
+
